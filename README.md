e91587f56369a270369fe0bc234ad8e438216aad
Dylan Reuter <dreuter@synercomm.com>
2022-03-18 16:57:39 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # XOR
-Python3 XOR encryption
+Python3 XOR encryption with rotating key
+
+## Features:
+* Supports a rotating integer or string encryption key.
+* Accepts binary file as input or pass it a string. 
+* Prints encrypted result to console as hex, or writes an encrypted binary file.
+* All output is null byte terminated.
+* Prints hex chars with no padding or '0' padding. e.g., 0x0 => 0x00
+
+## Usage:
+```bash
+usage: xor.py [-h] [-k K] [-f F] [-s S] [-w W]
+
+optional arguments:
+  -h, --help  show this help message and exit 
+  -k K        XOR key
+  -f F        bin file containing payload     
+  -s S        String to XOR
+  -w W        File to write encrypted binary payload to
+  --pad, --no-pad  Pad with 0 so all output is 2 chars wide 0x0 => 0x00 (default: --no-pad)
+  ```
+
+## Examples:
+```bash
+# Encrypt file with mysecretkey and print output in hex format
+python3 ./xor.py -f myfile.bin -k secretkey
+
+# Encrypt string
+python3 ./xor.py -s SomeFunctionCall -k secretkey
+
+# Encrypt binary file and output encrypted binary file
+python3 ./xor.py -f meterpreter.bin -k 1337 -w meterpreter.enc
+```
---
+++ xor.py
@@ -0,0 +1,57 @@
+# AUTHOR: Dylan Reuter (twitter: @dylanreuter4)
+
+#!/usr/bin/env python3
+
+import argparse
+import sys
+
+def xor(data, key):
+    res = b''
+    for i in range(len(data)):
+        curr_key = ord(key[i % len(key)])
+        res += bytes(chr(data[i] ^ curr_key), 'latin-1')
+        if i == len(data) - 1:
+            res += b'\0'
+    return res
+
+def print_output(ciphertext, pad):
+    padding = '02x' if pad == True else '1x'
+    print('{ 0x' + ', 0x'.join(format(x, padding) for x in ciphertext) + ' };')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='xor.py')
+    parser.add_argument(
+        '-k', help='XOR key'
+    )
+    parser.add_argument(
+       '-f', help='bin file containing payload' 
+    )
+    parser.add_argument(
+        '-s', help='String to XOR'
+    )
+    parser.add_argument(
+        '-w', help="File to write encrypted binary payload to"
+    )
+    parser.add_argument(
+        '--pad', dest='pad', help="Pad with 0 so all output is 2 chars wide 0x0 => 0x00", action=argparse.BooleanOptionalAction, default='--no-pad' 
+    )
+    args = parser.parse_args()
+
+    if args.f == None and args.s == None:
+        parser.print_help()
+        sys.exit()
+
+    if args.f != None:
+        with open(args.f, mode='rb') as file:
+            buf = file.read()
+            file.close()
+    else:
+        buf = bytes(args.s, 'latin-1')
+
+    ciphertext = xor(buf, args.k)
+    if args.w != None:
+        with open(args.w, mode='wb') as file:
+            file.write(ciphertext)
+            file.close
+    else:
+        print_output(ciphertext, args.pad)
